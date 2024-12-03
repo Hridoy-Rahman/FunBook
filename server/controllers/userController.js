599f910083453c1bd52d0d4eeb4eb70e72d90f78
Hridoy-Rahman <hridoyrahman715@gmail.com>
2024-12-03 17:54:39 +0600

    update user

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,6 @@
 import Verification from "../models/emailVerificationModel.js";
 import Users from "../models/userModel.js";
-import { compareString, hashString } from "../utils/index.js";
+import { compareString, createJWT, hashString } from "../utils/index.js";
 import passwordReset from "../models/passwordReset.js";
 import { resetPasswordLink } from "../utils/sendVerificationEmail.js";
 
@@ -176,3 +176,77 @@ export const changePassword = async (req, res, next) => {
       .json({ message: "An error occurred while changing the password." });
   }
 };
+
+export const getUser = async (req, res, next) => {
+  try {
+    const { userId } = req.body.user;
+    const { id } = req.params;
+
+    const user = await Users.findById(id ?? userId).populate({
+      path: "friends",
+      select: "-password",
+    });
+
+    if (!user) {
+      return res.status(200).send({
+        message: "User Not Found",
+        success: false,
+      });
+    }
+
+    user.password = undefined;
+
+    res.status(200).json({
+      success: true,
+      user: user,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "auth error",
+      success: false,
+      error: error.message,
+    });
+  }
+};
+
+export const updateUser = async (req, res, next) => {
+  try {
+    const { firstName, lastName, location, profileUrl, profession } = req.body;
+
+    if (!(firstName || lastName || profession || location)) {
+      next("Please provide all required fields");
+      return;
+    }
+
+    const { userId } = req.body.user;
+
+    const updateUser = {
+      firstName,
+      lastName,
+      location,
+      profileUrl,
+      profession,
+      _id: userId,
+    };
+    const user = await Users.findByIdAndUpdate(userId, updateUser, {
+      new: true,
+    });
+
+    await user.populate({ path: "friends", select: "-password" });
+    const token = createJWT(user?._id);
+
+    user.password = undefined;
+
+    res.status(200).json({
+      sucess: true,
+      message: "User updated successfully",
+      user,
+      token,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({ message: error.message });
+  }
+};
+
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -2,10 +2,13 @@ import express from "express";
 import path from "path";
 import {
   changePassword,
+  getUser,
   requestPasswordReset,
   resetPassword,
+  updateUser,
   verifyEmail,
 } from "../controllers/userController.js";
+import userAuth from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 const __dirname = path.resolve(path.dirname(""));
@@ -17,6 +20,11 @@ router.post("/request-passwordreset", requestPasswordReset);
 router.get("/reset-password/:userId/:token", resetPassword);
 router.post("/reset-password", changePassword);
 
+
+//user Routes
+
+router.post("/get-user/:id?",userAuth,getUser);
+router.put("/update-user",userAuth,updateUser)
 
 router.get("/verified", (req, res) => {
   res.sendFile(path.join(__dirname, "./views/build", "index.html"));
