3f7822463691cb082c19386a24256595e094754a
Hridoy-Rahman <hridoyrahman715@gmail.com>
2024-12-07 18:58:55 +0600

    get single post done

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -1,12 +1,15 @@
 import express from "express"
 import userAuth from "../middleware/authMiddleware.js"
-import { createPost, getPosts } from "../controllers/postController.js";
+import { createPost, getPosts, getSinglePost } from "../controllers/postController.js";
 
 const router = express.Router();
 
 // create post
 
 router.post("/create-post",userAuth,createPost)
-router.post("/get-post",userAuth,getPosts)
+
+router.post("/get-all-posts",userAuth,getPosts)
+
+router.post("/get-single-post",userAuth,getSinglePost)
 
 export default router;
